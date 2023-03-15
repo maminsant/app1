6d53dd29fc5b927d9c24aeab4ab0a16c44b7d3e5
20211TIIMI0141 <mamin8172@gmail.com>
2023-03-15 09:08:09 -0300

    Comentários explicativos

--- app/src/main/java/yasmin/santana/rodrigues/app1/MainActivity.java
+++ app/src/main/java/yasmin/santana/rodrigues/app1/MainActivity.java
@@ -15,15 +15,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button btnEnviar = findViewById(R.id.btnEnviar);
-        Intent i = new Intent(MainActivity.this, NextActivity.class);
-        btnEnviar.setOnClickListener(new View.OnClickListener() {
+        Button btnEnviar = findViewById(R.id.btnEnviar); //'linkando' com o btn da tela pelo id
+        btnEnviar.setOnClickListener(new View.OnClickListener() { //função para realizar o evento quando clica no botão
             @Override
-            public void onClick(View view) {
-                EditText etDigiteAqui = findViewById(R.id.etDigiteAqui);
-                String textoDigitado = etDigiteAqui.getText().toString();
-                i.putExtra("texto", textoDigitado);
-                startActivity(i);
+            public void onClick(View view) { //método - evento a ser realizado no botão
+                Intent i = new Intent(MainActivity.this, NextActivity.class); //cria a intent, qu pega informações da activity 1 e passa para 2.
+                EditText etDigiteAqui = findViewById(R.id.etDigiteAqui); //'linkando' com o edit text da tela pelo id
+                String textoDigitado = etDigiteAqui.getText().toString(); //pega o texto do et e converte para string
+                i.putExtra("texto", textoDigitado); //cria uma chave no dicionário com a chave texto, correspondente ao texto que a pessoa digitou
+                startActivity(i); //inicia evento na tela
             }
         });
     }
--- app/src/main/java/yasmin/santana/rodrigues/app1/NextActivity.java
+++ app/src/main/java/yasmin/santana/rodrigues/app1/NextActivity.java
@@ -12,9 +12,9 @@ public class NextActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_next);
-        Intent i = getIntent();
-        String textoDigitado = i.getStringExtra("texto");
-        TextView tvTexto = findViewById(R.id.tvText);
-        tvTexto.setText(textoDigitado);
+        Intent i = getIntent(); //cria a intent, qu pega informações da activity 1 e passa para 2.
+        String textoDigitado = i.getStringExtra("texto"); //texto que a pessoa digitou passa a corresponder aquela chave do dicionário criado na MAIN
+        TextView tvTexto = findViewById(R.id.tvText); //tv mostra na tela, 'linkando' com o tv da tela pelo id
+        tvTexto.setText(textoDigitado); //colocando esse texto na tela
     }
 }
